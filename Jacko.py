5bcbee57d0f2449fc752d53efb60381b9e2edfdc
pevveme <47577706+PinkytheDev@users.noreply.github.com>
2019-05-18 03:26:26 +0200

    Update Jacko.py

--- Jacko.py
+++ Jacko.py
@@ -78,14 +78,13 @@ async def ban(ctx, member : discord.Member, *, reason=None):
 async def unban(ctx, *, member):
     banned_users = await ctx.guild.bans()
     member_name, member_discriminator = member.split('a')
-
+    
     for ban_entry in banned_users:
         user = ban_entry.user
-
+        
         if (user.name, user.discriminator) == (member_name, member_discriminator):
             await ctx.guild.unban(user)
             await ctx.send(f"Unbanned {user.mention}")
             return
-
 
 client.run(str(os.environ.get('BOT_TOKEN')))
