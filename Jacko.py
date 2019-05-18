cf4a630791b6c9f504856ce20631edc853a7036e
pevveme <47577706+PinkytheDev@users.noreply.github.com>
2019-05-18 03:03:06 +0200

    Update Jacko.py

--- Jacko.py
+++ Jacko.py
@@ -76,19 +76,19 @@ async def ban(ctx, member : discord.Member, *, reason=None):
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('a')
+    try:
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('a')
 
-    for ban_entry in banned_users:
-        user = ban_entry.user
+        for ban_entry in banned_users:
+            user = ban_entry.user
 
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            try:
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
                 await ctx.send(f"Unbanned {user.mention}")
                 return
-            except:
-                await ctx.send("You don't have permission./ Failed")
+    except:
+        await ctx.send("You don't have permission. Or FAILED.")
 
 
 client.run(str(os.environ.get('BOT_TOKEN')))
