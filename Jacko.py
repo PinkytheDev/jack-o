fab96172f4c189c020b9f5c590f59648a3ca5cc8
pevveme <47577706+PinkytheDev@users.noreply.github.com>
2019-05-19 17:32:08 +0200

    Update Jacko.py

--- Jacko.py
+++ Jacko.py
@@ -9,7 +9,7 @@ client = commands.Bot(command_prefix='')
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name='MagicNoob Community | help'))
+    await client.change_presence(Game=discord.Game(name='MagicNoob Community | help'))
     print("On: True, Off: False")
 
 
